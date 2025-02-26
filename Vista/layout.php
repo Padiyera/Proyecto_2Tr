76d5e14367c4a2137b6828fc4a418359371c0578
Padiyera <ypadbor947@g.educaand.es>
2025-02-26 20:30:39 +0100

    2.2.Navbar

---
+++ Vista/layout.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html>
+
+<head>
+    <link rel="shortcut icon" href="#" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../Styles/main.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="../images/carV.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+            GruasJuan,S.L.
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="principal.php">Principal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="adminGrua.php">Admin Grua</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logs.php">Logs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="retiradas.php">Retiradas</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-4" id="content">
+        <!-- Content will be loaded here -->
+    </div>
+
+    <!-- jQuery, Popper.js, Bootstrap JS -->
+    <script src="../jquery/jquery-3.3.1.min.js"></script>
+    <script src="../popper/popper.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <!-- Custom JS -->
+    <script src="../Controlador/scripts.js"></script>
+</body>
+
+</html>
